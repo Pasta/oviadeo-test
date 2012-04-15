e96578d10338f3fa326f91f22e84b2adb9019cdf
Vincent Coste <coste.vincent@gmail.com>
2012-04-15 11:43:41 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OviadeoTest::Application.config.secret_token = '993b0acaca9915f87080ebbb8af633d8accafdf9052e237e393a1b85e1883da246f3106ee6e6e65acb24c0c621b7bf21a0b0fe52a4042562ec6704edc37d64c8'
